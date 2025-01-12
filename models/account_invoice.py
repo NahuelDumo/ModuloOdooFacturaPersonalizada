5481424f7189f01ba1e5d62ee87c7b137b2ef472
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-01-12 15:52:05 -0300

    actaulizacion codigo

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -3,10 +3,8 @@ from odoo import models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    @api.model
     def force_draft_invoices(self):
-        for invoice in self:
-            # Forzar el cambio de estado a 'draft' usando sudo para evitar restricciones
-            if invoice.state != 'draft':
-                invoice.sudo().button_draft()
+        # Aplicar el cambio de estado solo al registro actual (la factura que se está viendo)
+        if self.state != 'draft':
+            self.sudo().button_draft()
         return True
