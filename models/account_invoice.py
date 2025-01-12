119537839f68ee8df8713b16c4f96c19d1dc3659
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-01-12 15:46:18 -0300

    asd

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -3,7 +3,7 @@ from odoo import models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    @api.multi
+    @api.model
     def force_draft_invoices(self):
         for invoice in self:
             # Forzar el cambio de estado a 'draft' usando sudo para evitar restricciones
