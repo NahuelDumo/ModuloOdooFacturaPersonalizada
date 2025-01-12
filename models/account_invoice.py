d32bf6762e43d5280f25780324aac0e0da45e607
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-01-12 15:54:36 -0300

    cambio de cddigo

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -4,7 +4,8 @@ class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
     def force_draft_invoices(self):
-        # Aplicar el cambio de estado solo al registro actual (la factura que se está viendo)
-        if self.state != 'draft':
-            self.sudo().button_draft()
+        for invoice in self:
+            if invoice.state != 'draft':
+                # Forzar el cambio de estado a 'draft' sin más verificaciones
+                invoice.sudo().button_draft()
         return True
